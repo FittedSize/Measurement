8b05baae4c4d5e9cb484983f8cd40e0a8ad86901
folohunso <idrys01@hotmail.com>
2022-11-03 08:39:55 +0100

    feat: Add github actions

---
+++ .github/builds/lint.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+pip install flake8
+
+cat > setup.cfg << eof
+[flake8]
+extend-ignore = E203
+exclude = 
+	.git,
+	__pycache__,
+	docs/source/conf.py,old,build,dist,
+	manage.py,
+	migrations,
+	venv,
+	local_settings.py,
+	settings.py
+
+max-complexity = 10
+eof
+
+flake8
